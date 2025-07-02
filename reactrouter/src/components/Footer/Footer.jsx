166c5c022d1279ec4d3c8f127eff5e4932a4e4b8
Shubham Mourya <shubhammourya148@gmail.com>
2025-07-02 20:49:02 +0530

    Nav bar update

--- reactrouter/src/components/Footer/Footer.jsx
+++ reactrouter/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import Experiment from "../Logo/Experiment-logo.svg";
 
 export default function Footer() {
     return (
@@ -8,8 +9,8 @@ export default function Footer() {
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
                             <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                                className="mr-3 h-16"
+                                src={Experiment}
+                                className="mr-3 h-10 "
                                 alt="Logo"
                             />
                         </Link>
@@ -70,16 +71,17 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        © 2025
                         <a href="https://shubhammourya.com/" className=" hover:bg-orange-300 hover:text-black">
                             shubhammourya
                         </a>
                         . All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-                        <Link to="#" className="text-gray-500 hover:text-gray-900">
+                        {/* Social Media icons */}
+                        <Link to="#" className="text-gray-500 hover:text-yellow-600">
                             <svg
-                                className="w-4 h-4"
+                                className="w-4 h-4 "
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -93,7 +95,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">Facebook page</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="#" className="text-gray-500 hover:text-yellow-600">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
@@ -105,7 +107,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">Discord community</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="#" className="text-gray-500 hover:text-yellow-600">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
@@ -121,7 +123,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">Twitter page</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="#" className="text-gray-500 hover:text-yellow-600">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
@@ -137,7 +139,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">GitHub account</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="#" className="text-gray-500 hover:text-yellow-600">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -9,13 +9,21 @@ import {
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
-import Experiment from "./Logo/Experiment.svg";
+import User from "./components/User/User.jsx";
+import Github from ".//components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="about" element={<About />}/>
+     <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />} />
+      <Route path='user/' element={<User />} >
+        <Route path=':userid' element={<User />} />
+      </Route>
+      <Route 
+      path='github' 
+      element={<Github />} />
+      <Route path='*' element={<div>Not Found</div>} />
     </Route>
   )
 );
